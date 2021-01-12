a40856cb6676ed46e8fa3629eab88dcc556ec29d
tzofia <tzof0428@gmail.com>
2021-01-12 15:04:57 +0200

    build db

---
+++ insert_data.sql
@@ -0,0 +1,31 @@
+USE learnOnlineDB;
+insert into  Teachers(first_name, last_name, e_mail, phone, likes, gender, aviable_after_lesson) 
+values ("Gad", "Barak", "gadBarak@gmail.com", '0504120411' , 0, 'male', True );
+
+USE learnOnlineDB;
+insert into  Teachers(first_name, last_name, e_mail, phone, likes, gender, aviable_after_lesson) 
+values  ("Chana", "Coen", "chana@gmail.com", '0554412014' , 0, 'female', False );
+
+USE learnOnlineDB;
+insert into  Teachers(first_name, last_name, e_mail, phone, likes, gender, aviable_after_lesson) 
+values ("Mirian", "ben-shimon", "mBenS@gmail.com", '0522217745' , 0, 'female', True );
+
+
+
+insert into Subjects ( description, category)
+values ("Linear Algebra", 'Algebra'),
+        ("Boolean Algebra", 'Algebra'),
+        ("Article", 'English'),
+        ("Writing", 'English'),
+        ("c++", 'software'),
+        ("python", 'software');
+
+-- USE learnOnlineDB;
+-- insert into SubjectsForTechers(teacher_id, subject_id)
+-- values(10,1),
+--        (10,2),
+--        (10, 4),
+--        (11,3), 
+--        (11, 4),
+--        (12,5),
+--        (12,6)
