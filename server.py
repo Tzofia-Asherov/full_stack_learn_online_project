077bb0eb8e162236537fb62529790ed4cda21be6
RivkaO <rivkaover@gmail.com>
2021-01-13 01:17:07 +0200

    need to fix btn

--- server.py
+++ server.py
@@ -3,13 +3,13 @@ from model import teacher_model, subject_model
 
 import json
 
-app = Flask(__name__, static_url_path='', template_folder='template')
-# , static_folder='static'
+app = Flask(__name__, static_url_path='', static_folder='static', template_folder='template')
+# 
 
 @app.route('/')
 def root():
     subject_lst = subject_model.get_all()
-    return render_template('index.html', data={"name": "hellooooooooooooo", "subjects": subject_lst, "teachers": []})    
+    return render_template('index.html', data={"subjects": subject_lst, "teachers": []})    
 
 
 @app.route('/teachers', methods=["POST"])
@@ -37,7 +37,7 @@ def get_teachers():
     gender = request.args.get("gender")
     teacher_lst = teacher_model.get_by_subject(subject_id,gender)
     subject_lst = subject_model.get_all()
-    return render_template('pricing.html', data={"teachers": teacher_lst, "subjects": subject_lst })
+    return render_template('find_teacher.html', data={"teachers": teacher_lst, "subjects": subject_lst })
 
 
 if __name__ == "__main__":
